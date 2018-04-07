26b04618d77deaefbacd4085a2b47a7418c2fde5
Vishal Kumar <vishal.kumar@mountblue.io>
2018-04-07 11:48:54 +0530

    added message on login

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Link} from 'react-router-dom';
+import Header from './Header';
 
 class Dashboard extends React.Component{
     render(){
         return (
-            <h1>Dashboard</h1>
+            <div>
+                <Header />
+                <h1>Dashboard</h1>
+            </div>
         )
     }
 }
--- src/component/Signup.js
+++ src/component/Signup.js
@@ -4,10 +4,17 @@ import { Button } from 'semantic-ui-react';
 import {BrowserRouter as Router, Link} from 'react-router-dom';
 
 class Signup extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            successMessage : ''   
+        }
+    }
     createUser() {
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
+        
         let user = {
             name : name,
             email : email,
@@ -22,10 +29,11 @@ class Signup extends React.Component{
             })
         }).then((response) => response.json())
         .then((response) => {
-            // if(response.status === 'successful'){
-            //     window.location.href='/dashboard'
-            // }
-            console.log(response.status,response)
+            if(response.status === 'successful'){
+                this.setState({
+                    successMessage : 'Your account has been created successfully....',
+                })
+            }
         })
     }
     render(){
@@ -33,10 +41,11 @@ class Signup extends React.Component{
             <div className = 'container'>
                 <div id="signup-box">
                     <h1>Create an account</h1>
-                    <p className = 'label'>Enter your name : <input type='email' id='name'/></p>
-                    <p className = 'label'>Enter your email : <input type='email' id='email'/></p>
+                    <p className = 'label'>Enter your name : <input type='email' id='name' /></p>
+                    <p className = 'label'>Enter your email : <input type='email' id='email' /></p>
                     <p className = 'label'>Enter your Password : <input type = 'password' id='password'/></p>
                     <Button color ='twitter' fluid ={true} size = 'huge'  onClick={this.createUser.bind(this)} >Sign Up</Button>
+                    <div className = 'success-message'>{this.state.successMessage}</div>
                     <p className='query'>Already registered ? <Link to="/">login</Link></p>
                 </div>
             </div>
