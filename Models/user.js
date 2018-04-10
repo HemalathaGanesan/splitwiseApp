701f1691ceb8f56a7d8e6a99c8b85077658eebe0
Aman Kumar <aman.kumar@mountblue.io>
2018-04-10 10:17:07 +0530

    User schema modified

--- Models/user.js
+++ Models/user.js
@@ -1,27 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+//const establishConnection = require('../Connection/connection');
 
 const UserSchema = new Schema({
-    username:{
-        type:String,
-        required:true
-    },
-    email:{
-        type:String,
-        required:true
-    },
-    password:{
-        type:String,
-        required:true
-    },
-    mobile:Number,
-    friend:[],
-    paid:[{
-        to_whom:Number,
-		total_amount:Number,
-		description:String
-    }]
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  mobile: Number,
+  friends: [],
+  paid: [],
+  borrowed: []
 })
 
-const Users = mongoose.model('users',UserSchema);
+const Users = mongoose.model('users', UserSchema);
 module.exports = Users;
