833b883f718af93a70dc7751193046ce39f589fd
Aman Kumar <aman.kumar@mountblue.io>
2018-04-12 16:43:04 +0530

    group schema and modification in get friend api

--- routes/api.js
+++ routes/api.js
@@ -4,8 +4,8 @@ const User = require('../Models/user');
 
 //req{username,email,password}. return succeful or unsuccessful.
 router.post('/signup', function (req, res) {
-  User.findOne({ email: req.body.email }).then((data)=>{
-    if(data===null){
+  User.findOne({ email: req.body.email }).then((data) => {
+    if (data === null) {
       User.create({
         username: req.body.username,
         email: req.body.email,
@@ -15,7 +15,7 @@ router.post('/signup', function (req, res) {
           status: 'successful'
         })
       })
-    }else{
+    } else {
       res.send({
         status: 'Unsuccessful'
       })
@@ -31,7 +31,7 @@ router.post('/login', function (req, res) {
     if (email === data.email && password === data.password) {
       res.send({
         status: 'successful',
-        user_data:data
+        user_data: data
       })
     } else {
       res.send({
@@ -46,7 +46,7 @@ router.post('/login', function (req, res) {
 })
 
 //req{email} return user detail
-router.get('/user',function(){
+router.get('/user', function () {
   User.findOne({ email: friend_email }).then((data) => {
     if (data !== null) {
       res.send(data);
@@ -60,7 +60,7 @@ router.put('/addFriend', function (req, res) {
   var friend_email = req.body.friend_email;
   var user_email = req.body.user_email;
   User.findOne({ email: user_email }).then((data) => {
-    var user={};
+    var user = {};
     if (data !== null) {
       var friend_name = data.username;
       user = {
@@ -71,7 +71,7 @@ router.put('/addFriend', function (req, res) {
       res.send("Something Went wrong");
     }
     return user;
-  }).then((user)=>{
+  }).then((user) => {
     User.findOne({ email: friend_email }).then((data) => {
       if (data !== null) {
         var friend_name = data.username;
@@ -79,17 +79,16 @@ router.put('/addFriend', function (req, res) {
           "friend_name": friend_name,
           "friend_email": friend_email
         }
-        addFriendToFriendList(friend,user, user_email, friend_email).then((msg)=>{
-          console.log(msg, "msg");
+        addFriendToFriendList(friend, user, user_email, friend_email).then((msg) => {
           res.send({
             status: 'successful',
-            msg:msg
+            msg: msg
           })
         })
       } else {
         res.send({
           status: 'successful',
-          msg:"Your friend is not registered"
+          msg: "Your friend is not registered"
         })
       }
     })
@@ -100,10 +99,10 @@ router.put('/addFriend', function (req, res) {
 async function addFriendToFriendList(friend, user, user_email, friend_email) {
   var msg = '';
   var friend_id;
-  await User.findOne({email:friend_email}).then(data=>{
+  await User.findOne({ email: friend_email }).then(data => {
     friend_id = data._id
   })
-  console.log(friend_id,'friends ID');
+  console.log(friend_id, 'friends ID');
   await User.findOne({ email: user_email }).then((data) => {
     var user_id = data._id;
     var check;
@@ -150,15 +149,52 @@ async function addFriendToFriendList(friend, user, user_email, friend_email) {
 }
 
 //req{email} return friend list
-router.get('/friends/:email',function(req,res){
-  User.findOne({ email:req.params.email }).then((data) => {
+router.get('/friends/:email', function (req, res) {
+  var user_email = req.params.email
+  User.findOne({ email: user_email }).then((data) => {
     if (data !== null) {
-      res.send(data.friends);
+      var count = 0;
+      Promise.all(data.friends.map((frnd) => {
+        return splitBillWithFriend(user_email, frnd.friend_email).then((bill) => {
+          frnd.total = bill.total;
+          frnd.paid = bill.paid;
+          frnd.borrowed = bill.borrowed;
+          return frnd;
+        })
+      }))
+      .then( result => {
+         res.send(result);
+      })
+      
     } else {
       res.send("User not found");
     }
   })
 })
+
+//overall bill between user and specified friend.
+function splitBillWithFriend(user_email, friend_email) {
+  return User.findOne({ email: user_email }).then((data) => {
+    var lend = 0;
+    var borrowed = 0;
+    data.paid.forEach(function (element) {
+      if (element.friend_email === friend_email) {
+        lend = lend + element.lend;
+      }
+    })
+    data.borrowed.forEach(function (element) {
+      if (element.friend_email === friend_email) {
+        borrowed = borrowed + element.lend;
+      }
+    })
+    var bill = {
+      total: lend - borrowed,
+      paid: lend,
+      borrowed: borrowed
+    }
+    return bill;
+  })
+}
 
 // req{user_email,friend_email, description, amountPaid} divide the amount in 2.
 // udate the paid and borrowed feild of db.
@@ -169,10 +205,10 @@ router.post('/addBillWithFriend', function (req, res) {
   var description = req.body.description;
   User.findOne({ email: user_email }).then((data) => {
     var user = data;
-    if(user !== null){
-    var user_id = user._id;
+    if (user !== null) {
+      var user_id = user._id;
       User.findOne({ email: friend_email }).then((friend_data) => {
-        if(friend_data !== null){
+        if (friend_data !== null) {
           var friend_id = friend_data._id;
           var paid = {
             to_whom: friend_data.username,
@@ -209,56 +245,34 @@ router.post('/addBillWithFriend', function (req, res) {
               }
             }
           );
-          res.send('DB Updated');
-        }else{
+          res.send({
+            status: "Successful"
+          });
+        } else {
           res.send('Friend not found');
         }
       })
-    }else{
+    } else {
       res.send('user not found');
     }
   })
 })
 
-//
-router.get('/sharedBillWithFriend/:email/:frndMail',function(req,res){
-  User.findOne({email:req.params.email}).then((data)=>{
+//overall bill of a user
+router.get('/userBill/:email', function (req, res) {
+  User.findOne({ email: req.params.email }).then((data) => {
     var lend = 0;
     var borrowed = 0;
-    data.paid.forEach(function(element){
-      if(element.friend_email === req.params.frndMail){
-        lend = lend + element.lend;
-      }
-    })
-    data.borrowed.forEach(function(element){
-      if(element.friend_email === req.params.frndMail){
-        borrowed = borrowed+element.lend;
-      }
-    })
-    var bill = {
-      total:lend-borrowed,
-      paid:lend,
-      borrowed:borrowed
-    }
-    res.send(bill);
-  })
-})
-
-//
-router.get('/sharedBill/:email',function(req,res){
-  User.findOne({email:req.params.email}).then((data)=>{
-    var lend = 0;
-    var borrowed = 0;
-    data.paid.forEach(function(element){
+    data.paid.forEach(function (element) {
       lend = lend + element.lend;
     })
-    data.borrowed.forEach(function(element){
-      borrowed = borrowed+element.lend;
+    data.borrowed.forEach(function (element) {
+      borrowed = borrowed + element.lend;
     })
     var bill = {
-      total:lend-borrowed,
-      paid:lend,
-      borrowed:borrowed
+      total: lend - borrowed,
+      paid: lend,
+      borrowed: borrowed
     }
     res.send(bill);
   })
@@ -266,10 +280,10 @@ router.get('/sharedBill/:email',function(req,res){
 
 //req{email} return all expense for that user
 
-router.get('/allExpenses/:email',function(req,res){
-  User.findOne({email:req.params.email}).then((data)=>{
-    if(data != null){
-      var expense_list = [...data.paid,...data.borrowed];
+router.get('/allExpenses/:email', function (req, res) {
+  User.findOne({ email: req.params.email }).then((data) => {
+    if (data != null) {
+      var expense_list = [...data.paid, ...data.borrowed];
       res.send(expense_list);
     }
   })
